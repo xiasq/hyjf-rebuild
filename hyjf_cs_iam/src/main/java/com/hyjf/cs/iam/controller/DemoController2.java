76b6d8cc5656aa652664b9af389e766722faba54
xiasq <mx2012xshq@126.com>
2018-04-13 11:53:59 +0800

    调整项目名称

--- hyjf_cs_iam/src/main/java/com/hyjf/cs/iam/controller/DemoController2.java
+++ hyjf_cs_iam/src/main/java/com/hyjf/cs/iam/controller/DemoController2.java
@@ -3,6 +3,7 @@ package com.hyjf.cs.iam.controller;
 import com.hyjf.cs.iam.service.IamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class DemoController2 {
     @Autowired
     private IamService iamService;
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String demo1(){
         iamService.getUserById(1);
         return "demo2";
--- hyjf_eureka/src/main/java/com/hyjf/eureka/EurekaApplication.java
+++ hyjf_eureka/src/main/java/com/hyjf/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.hyjf;
+package com.hyjf.eureka;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
